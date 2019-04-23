61004cf726221ef058bc0a07db4ade9139644394
Paul Lorett Amazona <whatevergeek@gmail.com>
2019-04-24 02:37:26 +0800

    introduce issue (in comments)

--- csharp_netcore_pbt_demo/Arithmetic.cs
+++ csharp_netcore_pbt_demo/Arithmetic.cs
@@ -7,5 +7,9 @@ namespace csharp_netcore_pbt_demo
     public static class Arithmetic
     {
         public static int Multiply(int x, int y) => x * y;
+
+        // Introduce Issue
+        //public static int Multiply(int x, int y) => 
+        //    x == -10 || y == 10 ? throw new Exception("sample issue") :x * y;
     }
 }
